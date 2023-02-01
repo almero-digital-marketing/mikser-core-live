2863c8062c5ea75fb1ad5fcf2b6b54a8ef063372
dimitarkolev <dk@almero.bg>
2023-02-01 12:38:07 +0000

    Changed plugin interface

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import { onLoaded, mikser, useLogger } from 'mikser-core'
 import liveServer from 'alive-server'
 
-onLoaded(async () => {
-    const logger = useLogger()
-    logger.info('Starting live server')
-    liveServer.start({
-        wait: 1000,
-        root: mikser.options.outputFolder,
-        open: false,
-        ...mikser.config.live, 
+export default ({ onLoaded, mikser, useLogger }) => {
+    onLoaded(async () => {
+        const logger = useLogger()
+        logger.info('Starting live server')
+        liveServer.start({
+            wait: 1000,
+            root: mikser.options.outputFolder,
+            open: false,
+            ...mikser.config.live, 
+        })
     })
-})
+}
